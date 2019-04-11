16bae20c3ccac8c765539332806c168917a5bc45
garchaaman19 <garchaaman19@gmail.com>
2019-04-11 09:22:25 +0000

    Delete action_admission_mcs.py

--- action_admission_mcs.py
+++
@@ -1,8 +0,0 @@
-from rasa_core.actions.action import Action
-from rasa_core.events import Slotset
-class AdmissionMcs(Action):
-	def name(self):
-		return 'admission_mcs'
-	def run(self,dispatcher,tracker,domain):
-		stream=tracker.get_slot('stream')
-		  
